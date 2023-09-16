efc8570fca9a93d84caf99c8e2ffd1bd16e4b849
some-molasses <river.stanley173@gmail.com>
2023-09-16 02:11:21 -0400

    frontend start

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -12,6 +12,10 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
+      <nav>
+        <ul>
+        </ul>
+      </nav>
       <main className={`${styles.main} ${inter.className}`}>
 
       </main>
