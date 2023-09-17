076e0485de68f64544f665e97f8e4b70b39904d6
some-molasses <river.stanley173@gmail.com>
2023-09-17 01:55:23 -0400

    reflect updates

--- frontend/src/app/reflect/page.tsx
+++ frontend/src/app/reflect/page.tsx
@@ -16,7 +16,31 @@ export const ReflectContent: React.FC<{}> = () => {
 };
 
 const ReflectionSection: React.FC<{ title: string }> = ({ title }) => {
-  return <h1>{title}</h1>;
+  return (
+    <div className={styles.reflectionSection}>
+      <div className="icon">
+        {/* eslint-disable-next-line @next/next/no-img-element */}
+        <img src="https://placekitten.com/g/400/400" alt=""></img>
+      </div>
+      <div className="description">
+        <h1>Relationships</h1>
+        <p>Your relationships are on the rocks!</p>
+        <p>
+          In the past week, you have had a 147% increase in negative comments
+          about your relationships.
+        </p>
+        <ul className="quotes">
+          <li>I hate the way she cooks potatoes</li>
+          <li>Potatoes!! Should be neither green!!! Nor meaty!!!</li>
+        </ul>
+        <div className="conclusion">
+          <span className="conclusion-type">Opportunity for reflection</span>
+          <span>Why do you think this pattern has emerged?</span>
+          <button>Journal</button>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default ReflectContent;
